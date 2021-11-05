fd316cf1e96f898dbe8004727820890b30dd4169
gongh95 <gonzalo.simon.1995@gmail.com>
2021-11-04 22:24:06 -0300

    4/11-22:23

---
+++ codigo.js
@@ -0,0 +1,11 @@
+// var frase1 = "Hola ";
+// var nombre = prompt("Escribi tu nombre");
+// alert(frase1 + nombre + ", como estás?");
+// __________________________________
+// let numero1 = 25;
+// let numero2 = 30;
+
+// let afirmacion1 = numero1 < numero2;
+// let afirmacion2 = numero1 != numero2;
+
+// alert(!afirmacion1)
